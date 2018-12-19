4edf57e416efc53bab016ce32dae147c077d65f4
Feathergunner <na87vix@ppc680.mirz.uni-jena.de>
2018-12-19 18:53:55 +0100

    Added string output for measurement result stats

--- run_time_eval.py
+++ run_time_eval.py
@@ -4,11 +4,24 @@
 import time
 
 class measurement_result:
-	def __init__(self, algorithm, algo_input, algo_output, running_time):
-		self.algorithm_name = algorithm
+	def __init__(self, algorithm_name, algo_input, algo_output, running_time):
+		self.algorithm_name = algorithm_name
 		self.input = algo_input
 		self.output = algo_output
 		self.running_time = running_time
+
+	def __str__(self):
+		string =  "NAME:         "+self.algorithm_name+"\n"
+		if isinstance(self.input, list):
+			string += "INPUT:        "+str([str(item) for item in self.input])+"\n";
+		else:
+			string += "INPUT:        "+str(self.input)+"\n"
+		if isinstance(self.output, list):
+			string += "OUTPUT:       "+str([str(item) for item in self.output])+"\n";
+		else:
+			string += "OUTPUT:       "+str(self.output)+"\n"
+		string += "RUNNING TIME: "+str(self.running_time)+" sec."
+		return string
 
 def measure_running_time(algo_function, algo_input):
 	start = time.time()
@@ -16,4 +29,4 @@ def measure_running_time(algo_function, algo_input):
 	end = time.time()
 
 	running_time = end-start
-	return measurement_result(algorithm, algo_input, result, running_time)
+	return measurement_result(algo_function.__name__, algo_input, result, running_time)
