d448fcc01c9db85e28274de312afc777f0fe1d9f
Andreas Goral <angohz@ymail.com>
2019-03-10 17:51:30 +0100

    Added method to construct all tables for compare deterministic and random variants

--- main_plots.py
+++ main_plots.py
@@ -2,10 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from Evaluation import PlotConstructor as pc
+from Evaluation import TableConstructor as tc
+from Evaluation import StatisticsManager as sm
+import os
+from subprocess import call
 
 def make_plots():
-	#plot_performance_by_algo_boxplots()
-	#plot_performance_by_algo_lineplots()
+	plot_performance_by_algo_boxplots()
+	plot_performance_by_algo_lineplots()
 	plot_performance_by_density()
 
 def plot_performance_by_algo_boxplots():
@@ -24,8 +28,21 @@ def plot_performance_by_density():
 	for setname in ["general", "planar", "maxdeg", "maxclique"]:
 		pc.make_performance_plots_all(setname, axis="TIME", type="ABSOLUTE")
 		pc.make_performance_plots_all(setname, axis="OUTPUT", type="RP")
+
+def build_all_tables_detrand_compare():
+	for setname in ["general", "planar", "maxdeg", "maxclique"]:
+		outputfilenames = []
+		for density_class in ["dense", "sparse"]:
+			if not (setname == "planar" and density_class == "dense"):
+				print (setname+", "+density_class)
+				tc.construct_table_compare_randomized(setname, density_class)
+				outputfilenames.append("table_cmprand_"+setname+"_"+density_class)
 	
+		os.chdir("data/eval/random_"+setname+"/tables")
+		for tablefile in outputfilenames:
+			call(["pdflatex",tablefile+".tex"])
+		os.chdir("../../../..")
 
 if __name__ == "__main__":
 	#make_plots()
-	pc.performance_plot_analyze_reduction("general", "CMT")
+	build_all_tables_detrand_compare()
