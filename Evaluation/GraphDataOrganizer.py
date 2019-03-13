5399009b75933fe65b58cb3a2cd0c9b5681a979d
Andreas Goral <angohz@ymail.com>
2019-03-13 13:22:42 +0100

    Removed debug output

--- Evaluation/GraphDataOrganizer.py
+++ Evaluation/GraphDataOrganizer.py
@@ -180,10 +180,7 @@ def construct_set_random_graph(parameters, force_new_data=False):
 	elif "rel_m" in parameters:
 		subclass = "sparse"
 		p = round((2*parameters["rel_m"]*n)/(2*(n*(n-1))),2)
-	
-	print (parameters)
-	print (p)
-	
+		
 	graphdir = "data/eval/random_"+graphclass+"/input/"
 	check_filepath(graphdir)
 	
